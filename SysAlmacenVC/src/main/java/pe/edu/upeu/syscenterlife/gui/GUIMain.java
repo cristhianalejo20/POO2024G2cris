8150b257f7d6db49bd640c871770c16f6933036e
TheReeds <113415034+TheReeds@users.noreply.github.com>
2024-05-13 11:06:11 -0500

    jajaj

--- SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/gui/GUIMain.java
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/gui/GUIMain.java
@@ -147,7 +147,7 @@ public class GUIMain extends JFrame {
             if (((JMenuItem) e.getSource()).getName()
                     .equals("micliente")) {
                 System.out.println("Holas si llega");
-                /*jtpane.removeAll();
+                jtpane.removeAll();
 //MainCliente mc = new MainCliente();
 MainCliente mc = ctx.getBean(MainCliente.class);
 mc.setContexto(ctx);
@@ -160,7 +160,7 @@ JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 jtpane.add(scrollPane, "Cliente");
 contai.add(BorderLayout.CENTER, jtpane);
 contai.validate();
-contai.repaint();*/
+contai.repaint();
             }
             if (((JMenuItem) e.getSource()).getName().equals("miareaperiodo")) {
                 System.out.println("Si llega!");
---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/repositorio/ClienteRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upeu.syscenterlife.modelo.Cliente;
+@Repository
+public interface  ClienteRepository extends
+    
+    JpaRepository<Cliente, String>{
+   
+}
+        
