7238d705cbcceda468b20ea5a49ee6a7365b7d3f
RennzoxD <cardenascv590@gmail.com>
2024-05-20 11:07:31 -0500

    trabajo

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Categoria.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.modelo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
+    @JsonIgnoreProperties({"idCategoria"})
+    public List<Producto> productos;
+}
--- SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/repositorio/ClienteRepository.java
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/repositorio/ClienteRepository.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pe.edu.upeu.syscenterlife.repositorio;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upeu.syscenterlife.modelo.Cliente;
 @Repository
 public interface  ClienteRepository extends
     
     JpaRepository<Cliente, String>{
+    @Query(value = "SELECT * FROM cliente WHERE nombrers like :nombre", nativeQuery = true)
+    List<Cliente> findByNombre(@Param(value ="nombre") String nombre);
    
 }
         
